b2d03ce75c7832b88ee226df1296e592b42d3d39
Lawson Owhorji <lawsonowhorji2020@gmail.com>
2025-01-18 16:48:05 +0100

    update terraform resource files

--- bucket.tf
+++ bucket.tf
@@ -4,3 +4,17 @@ resource "google_storage_bucket" "gcp-bucket" {
   storage_class = "STANDARD"
   uniform_bucket_level_access = true
 }
+
+resource "google_storage_bucket" "gcp-bucket1" {
+  name     = "nexedge-bucket-lawsonbucket1"
+  location = "EUROPE-WEST1"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+}
+
+resource "google_storage_bucket" "gcp-bucket2" {
+  name     = "nexedge-bucket-lawsonbucket2"
+  location = "EUROPE-NORTH1"
+  storage_class = "STANDARD"
+  uniform_bucket_level_access = true
+}
--- db-instance.tf
+++ db-instance.tf
@@ -8,3 +8,25 @@ resource "google_sql_database_instance" "instance" {
 
   deletion_protection  = "false"
 }
+
+resource "google_sql_database_instance" "instance1" {
+  name             = "mysql-db"
+  region           = "europe-west1"
+  database_version = "MYSQL_8_0"
+  settings {
+    tier = "db-f1-micro"
+  }
+
+  deletion_protection  = "false"
+}
+
+resource "google_sql_database_instance" "postgres_instance" {
+  name             = "postgres-instance"
+  database_version = "POSTGRES_13"
+  region           = "us-central1"
+  settings {
+    tier = "db-f1-micro"
+  }
+  
+  deletion_protection = false
+}
